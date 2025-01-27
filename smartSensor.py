e6bdf00ca4f3ddcd0f9c98262fd7c5e9ac53af56
Gage M. Smith <60896044+gagesmith3@users.noreply.github.com>
2025-01-27 14:40:41 -0500

    Update smartSensor.py

--- smartSensor.py
+++ smartSensor.py
@@ -10,11 +10,19 @@ GPIOpin = -1
 # Initial the input pin
 def initialInductive(pin):
   global GPIOpin
+  global mysqli
+  global mycursor
   GPIOpin = pin
   GPIO.setmode(GPIO.BCM)
   GPIO.setup(GPIOpin,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
   print("Finished GPIO Initiation")
-
+  mysqli=mysql.connector.connect(
+    host='192.168.1.54',
+    user='webapp',
+    password='STUDS2650',
+    database='iwt_db')
+  mycursor = mysqli.cursor()
+  print("Finished mySQLi Initiation")
 
 # Detect Metal
 def detectMetal():
@@ -29,41 +37,57 @@ def detectMetal():
          oldState=newState
        else:
          oldState=newState
-
    else:
      print("Please Initial Input Ports")
 
+
+#sendStatus
+def sendStatus():
+  global mysqli
+  global mycursor
+  global count
+  if(count >= 1):
+    status='ACTIVE'
+  else:
+    status='INACTIVE'
+  sql = "UPDATE heading_data SET headStatus = %s WHERE headID = %s"
+  val = (status, '10')
+  mycursor.execute(sql,val)
+  mysqli.commit()
+  print("Header Status Updated")
+
+
 #sendData
 def sendData():
-  mysqli=mysql.connector.connect(
-        host='192.168.1.54',
-        user='webapp',
-        password='STUDS2650',
-        database='iwt_db')
-
   global count
+  global mysqli
+  global mycursor
   x = datetime.datetime.now()
   myDate = x.strftime("%x")
   myHour = x.strftime("%H")
   myMin = x.strftime("%M")
-  mycursor=mysqli.cursor()
   sql = "INSERT INTO heading_rates (headName, studCount, updateFullDate, updateDate, updateHour, updateMinute) VALUES (%s, %s, %s, %s, %s, %s)"
   val = ("SP11", count, x, myDate, myHour, myMin)
   mycursor.execute(sql,val)
   mysqli.commit()
-  print("Record Inserted")
+  print("Count Record Inserted")
   count = 0
 
-
-
-# test module
+# main module
 if __name__ == '__main__':
   pin = 17
   count = -1
+  upTime = -1
+  downTime = -1
+  mysqli = 'mysqli'
+  mycursor = 'mycursor'
   initialInductive(pin)
   oldState = 2
+  schedule.every(1).minutes.do(sendStatus)
   schedule.every(1).minutes.do(sendData)
   print("Count Sequence Started")
   while True:
     detectMetal()
     schedule.run_pending()
+
+
