110cb5348596674db7d80345a8f43cbd37c6eec8
Gage M. Smith <60896044+gagesmith3@users.noreply.github.com>
2025-01-27 08:30:35 -0500

    Create smarterSensor.py

---
+++ smarterSensor.py
@@ -0,0 +1,89 @@
+import time
+import RPi.GPIO as GPIO
+import mysql.connector
+import schedule
+import datetime
+
+# Pin of Input
+GPIOpin = -1
+
+# Initial the input pin
+def initialInductive(pin):
+  global GPIOpin
+  global mysqli
+  global mycursor
+  GPIOpin = pin
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(GPIOpin,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+  print("Finished GPIO Initiation")
+  mysqli=mysql.connector.connect(
+    host='192.168.1.54',
+    user='webapp',
+    password='STUDS2650',
+    database='iwt_db')
+  mycursor = mysqli.cursor()
+  print("Finished mySQLi Initiation")
+
+
+# Detect Metal
+def detectMetal():
+   global oldState
+   global count
+   global downTime
+   global upTime
+   if(GPIOpin != -1):
+     newState = GPIO.input(GPIOpin)
+     if newState != oldState:
+       if newState==1:
+         count+=1
+         print(count)
+         oldState=newState
+       else:
+         oldState=newState
+     else:
+       downTime+=1
+       print(downTime)
+
+   else:
+     print("Please Initial Input Ports")
+
+#sendUpTime
+def sendUpTime():
+  print("UPTIME")
+
+
+#sendData
+def sendData():
+  global count
+  global mysqli
+  global mycursor
+  x = datetime.datetime.now()
+  myDate = x.strftime("%x")
+  myHour = x.strftime("%H")
+  myMin = x.strftime("%M")
+  mycursor=mysqli.cursor()
+  sql = "INSERT INTO heading_rates (headName, studCount, updateFullDate, updateDate, updateHour, updateMinute) VALUES (%s, %s, %s, %s, %s, %s)"
+  val = ("TEST2", count, x, myDate, myHour, myMin)
+  mycursor.execute(sql,val)
+  mysqli.commit()
+  print("Count Record Inserted")
+  count = 0
+
+
+
+# test module
+if __name__ == '__main__':
+  pin = 17
+  count = -1
+  upTime = 0
+  downTime = 0
+  mysqli = 'test'
+  mycursor = 'test'
+  initialInductive(pin)
+  oldState = 2
+  schedule.every(1).minutes.do(sendData)
+  schedule.every(1).minutes.do(sendUpTime)
+  print("Count Sequence Started")
+  while True:
+    detectMetal()
+    schedule.run_pending()
